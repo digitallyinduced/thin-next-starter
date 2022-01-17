87851db800255f96a8064ad92d23f804984e7929
Marc Scholten <marc@digitallyinduced.com>
2022-01-17 14:05:48 +0100

    Fixed initial build

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import { AuthUser, ensureIsUser, getCurrentUser, useCurrentUser } from 'ihp-backend';
 import { initIHPBackend } from 'ihp-datasync';
 import { useQuery } from 'ihp-datasync/react';
-import { Post, query } from 'ihp-datasync';
+import { query } from 'ihp-datasync';
 import { useEffect, useState } from 'react';
 
 initIHPBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
@@ -18,10 +18,10 @@ function Blog({}) {
 
     return <div>
         <div>Hello {isLoggedIn?.email}</div>
-        <Posts/>
     </div>
 }
 
+/* EXAMPLE:
 function Posts() {
     const posts = useQuery(query('posts'));
     if (posts === null) {
@@ -37,5 +37,6 @@ interface PostProps {
 function Post({ post }: PostProps) {
     return <div>{post.title}</div>
 }
+*/
 
 export default Blog;
