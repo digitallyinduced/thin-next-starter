7a1995d5cef84ea0536969a2fd9186f41cce4913
Marc Scholten <marc@digitallyinduced.com>
2022-05-24 15:47:27 +0200

    Updated to latest thin-backend version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { initThinBackend } from 'thin-backend';
 import { ThinBackend } from 'thin-backend/react';
+import 'thin-backend-react/auth.css';
 
 initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
 
